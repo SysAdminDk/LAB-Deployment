27e06f6e3d31381bede17c5707a1dc1554264f58
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2025-09-15 17:54:45 +0200

    Update ADDS-01.ps1

--- Windows Servers/ADDS-01.ps1
+++ Windows Servers/ADDS-01.ps1
@@ -111,12 +111,7 @@ if (!((gwmi win32_computersystem).partofdomain)) {
     
 	    Rename-Computer -NewName "ADDS-01" -Restart
     
-        Write-Host "Wait for Server Rename"
-        for ($i=0; $i -le 300; $i++) {
-            Write-Host "." -NoNewline
-            Start-Sleep -Seconds 1
-        }    
-        break
+        Start-Sleep -Seconds 300
     }
 
 
@@ -148,19 +143,15 @@ if (!((gwmi win32_computersystem).partofdomain)) {
 
         # Wait for Restart
         # --------------------------------------------------------------------------------------------------
-        Write-Host "Wait for Domain Reboot"
-        for ($i=0; $i -le 300; $i++) {
-            Write-Host "." -NoNewline
-            Start-Sleep -Seconds 1
-        }    
-        break
+        Start-Sleep -Seconds 300
     }
+
 }
 
 
 # Run after Domain have been created.
 # --------------------------------------------------------------------------------------------------
-if ((gwmi win32_computersystem).partofdomain) {
+if ((gwmi win32_computersystem).DomainRole -eq 5) {
 
     # Add Reverse lookup DNS Zone
     # --------------------------------------------------------------------------------------------------
@@ -306,6 +297,50 @@ if ((gwmi win32_computersystem).partofdomain) {
     if ( (!(Test-Path -Path "C:\Windows\SYSVOL\domain\scripts\DSC-Files")) -and ($null -ne $DSCFiles) ) {
         New-Item -Path "C:\Windows\SYSVOL\domain\scripts\DSC-Files" -ItemType Directory | Out-Null
         $DSCFiles | ForEach-Object { Copy-Item $_.FullName -Destination "$VHDXVolume3\Scripts\MOF\" }
+    }
+
+
+
+    <#
+
+        System State - AD Backup
+
+    #>
+    if ((Get-Disk).count -gt 1) {
+
+        # Ensure Windows Server Backup is installed
+        # ------------------------------------------------------------
+        if (!(Get-Command Start-WBBackup -ErrorAction SilentlyContinue)) {
+            Install-WindowsFeature -Name Windows-Server-Backup -IncludeManagementTools
+        }
+
+        # Setup Scheduled backup
+        # ------------------------------------------------------------
+        $Disk = Get-WBDisk | Where { $_.TotalSpace -gt (Get-Partition | Where {$_.DriveLetter -eq "C"} | Get-Disk).Size }
+        if ($Disk.count -gt 1) {
+            throw "Multiple disks found, please ensure there is only one"
+            break
+        }
+        $DiskInfo = Get-Disk -Number $Disk.DiskNumber
+
+        if ($DiskInfo.OperationalStatus -ne "Online") {
+            Get-Disk -Number $Disk.DiskNumber | Set-Disk -IsOffline:$False
+            $DiskInfo | Initialize-Disk
+            $DiskInfo | Clear-Disk -Confirm:0
+        }
+
+        if ($null -ne $Disk) {
+                
+            if (!(Get-WBPolicy)) {
+                & wbadmin enable backup -addtarget:"{$($Disk.DiskId.Guid)}" -Schedule:22:00 -allCritical -quiet
+            } else {
+                Write-Warning "Backup already configured, please check configuration."
+                Get-WBPolicy
+            }
+
+        } else {
+            Write-Warning "No AD Backup configured"
+        }
     }
 
 
